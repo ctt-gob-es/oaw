3af409df9c06b4eea7620aa11af9d5b4f7be3f7e
mikunis <miguel.garcia@fundacionctic.org>
2016-08-18 09:52:51 +0200

    Fixes send email by mdm.correo.gob.es smtp server

--- crawler/src/main/java/es/inteco/utils/MailUtils.java
+++ crawler/src/main/java/es/inteco/utils/MailUtils.java
@@ -3,11 +3,12 @@ package es.inteco.utils;
 import es.inteco.common.logging.Logger;
 import es.inteco.common.properties.PropertiesManager;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.EmailException;
 import org.apache.commons.mail.MultiPartEmail;
 
+import javax.net.ssl.*;
 import java.net.URL;
 import java.util.List;
 
@@ -16,18 +17,12 @@ public final class MailUtils {
     private MailUtils() {
     }
 
-    public static void sendMail(String mailFrom, String mailFromName, List<String> mailTo, String subject, String text, String attachUrl, String attachName, String replyTo, String replyToName, boolean isError) {
+    public static void sendMail(String fromAddress, String fromName, List<String> mailTo, String subject, String text, String attachUrl, String attachName, String replyTo, String replyToName, boolean isError) {
         try {
             // Create the email message
             final MultiPartEmail email = createEmail();
 
-            for (String addressStr : mailTo) {
-                email.addBcc(addressStr);
-            }
-
-            email.setFrom(mailFrom, mailFromName);
-            email.setSubject(subject);
-            email.setMsg(text);
+            setEmailInfo(email, mailTo, fromAddress, fromName, subject, text);
 
             if (StringUtils.isNotEmpty(replyTo) && StringUtils.isNotEmpty(replyToName)) {
                 email.addReplyTo(replyTo, replyToName);
@@ -38,7 +33,6 @@ public final class MailUtils {
                 EmailAttachment attachment = new EmailAttachment();
                 attachment.setPath(attachUrl);
                 attachment.setDisposition(EmailAttachment.ATTACHMENT);
-                // attachment.setDescription("Informe PDF");
                 attachment.setName(attachName);
                 // add the attachment
                 email.attach(attachment);
@@ -47,22 +41,16 @@ public final class MailUtils {
             // send the email
             email.send();
         } catch (Exception e) {
-            Logger.putLog("No se ha podido enviar el correo MailUtils.sendMail.", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
+            Logger.putLog("FALLO No se ha podido enviar el correo", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
         }
     }
 
-    public static void sendExecutiveMail(String mailFrom, String mailFromName, List<String> mailTo, String subject, String text, URL attachUrl, String attachName, String replyTo, String replyToName, boolean isError) {
+    public static void sendExecutiveMail(String fromAddress, String fromName, List<String> mailTo, String subject, String text, URL attachUrl, String attachName, String replyTo, String replyToName, boolean isError) {
         try {
             // Create the email message
             final MultiPartEmail email = createEmail();
 
-            for (String addressStr : mailTo) {
-                email.addBcc(addressStr);
-            }
-
-            email.setFrom(mailFrom, mailFromName);
-            email.setSubject(subject);
-            email.setMsg(text);
+            setEmailInfo(email, mailTo, fromAddress, fromName, subject, text);
 
             if (StringUtils.isNotEmpty(replyTo) && StringUtils.isNotEmpty(replyToName)) {
                 email.addReplyTo(replyTo, replyToName);
@@ -73,7 +61,6 @@ public final class MailUtils {
                 EmailAttachment attachment = new EmailAttachment();
                 attachment.setURL(attachUrl);
                 attachment.setDisposition(EmailAttachment.ATTACHMENT);
-                // attachment.setDescription("Informe PDF");
                 attachment.setName(attachName);
                 // add the attachment
                 email.attach(attachment);
@@ -82,7 +69,7 @@ public final class MailUtils {
             // send the email
             email.send();
         } catch (Exception e) {
-            Logger.putLog("No se han podido enviar los resultados del rastreo por correo.", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
+            Logger.putLog("FALLO No se han podido enviar los resultados del rastreo por correo.", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
         }
     }
 
@@ -91,40 +78,103 @@ public final class MailUtils {
             // Create the email message
             final MultiPartEmail email = createEmail();
 
-            for (String addressStr : mailsTo) {
-                email.addBcc(addressStr);
-            }
-
-            email.setFrom(fromAddress, fromName);
-            email.setSubject(subject);
-            email.setMsg(text);
+            setEmailInfo(email, mailsTo, fromAddress, fromName, subject, text);
 
             // send the email
             email.send();
         } catch (Exception e) {
-            Logger.putLog("No se han podido enviar el correo correo.", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
+            Logger.putLog("FALLO No se han podido enviar el correo simple", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
         }
+    }
+
+    private static void setEmailInfo(final MultiPartEmail email, final List<String> mailTo, final String mailFrom, final String mailFromName, final String subject, final String text) throws EmailException {
+        for (String addressStr : mailTo) {
+            email.addBcc(addressStr);
+        }
+
+        email.setFrom(mailFrom, mailFromName);
+        email.setSubject(subject);
+        email.setMsg(text);
     }
 
     private static MultiPartEmail createEmail() throws Exception {
         final MultiPartEmail email = new MultiPartEmail();
         final PropertiesManager pmgr = new PropertiesManager();
 
+        final String trustStorePath = pmgr.getValue("certificados.properties", "truststore.path");
+        final String trustStorePass = pmgr.getValue("certificados.properties", "truststore.pass");
+        if ( trustStorePath!=null && !trustStorePath.isEmpty()) {
+            System.setProperty("javax.net.ssl.trustStore", trustStorePath);
+        }
+        if ( trustStorePass!=null && !trustStorePass.isEmpty()) {
+            System.setProperty("javax.net.ssl.trustStorePassword", trustStorePass);
+        }
+
+        final TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+        };
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            SSLContext.setDefault(sc);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            Logger.putLog("Excepción: ", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
+        }
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            SSLContext.setDefault(sc);
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+        } catch (Exception e) {
+            Logger.putLog("Excepción: ", MailUtils.class, Logger.LOG_LEVEL_ERROR, e);
+        }
+
         if (pmgr.getValue("crawler.core.properties", "mail.smtp.host") == null || pmgr.getValue("crawler.core.properties", "mail.smtp.host").trim().isEmpty()) {
             throw new Exception("No se configurado el servidor de correo");
         }
         email.setHostName(pmgr.getValue("crawler.core.properties", "mail.smtp.host"));
         if (pmgr.getValue("crawler.core.properties", "mail.smtp.user") != null && !pmgr.getValue("crawler.core.properties", "mail.smtp.user").trim().isEmpty()) {
-            email.setSSL(true);
-            email.setAuthentication(pmgr.getValue("crawler.core.properties", "mail.smtp.user").trim(),pmgr.getValue("crawler.core.properties", "mail.smtp.pass").trim());
+            email.setAuthenticator(new DefaultAuthenticator(pmgr.getValue("crawler.core.properties", "mail.smtp.user").trim(), pmgr.getValue("crawler.core.properties", "mail.smtp.pass").trim()));
         }
         if (pmgr.getValue("crawler.core.properties", "mail.smtp.port") != null && !pmgr.getValue("crawler.core.properties", "mail.smtp.port").trim().isEmpty()) {
+            Logger.putLog("Configurando el port " + pmgr.getValue("crawler.core.properties", "mail.smtp.port"), MailUtils.class, Logger.LOG_LEVEL_INFO);
             email.setSmtpPort(Integer.parseInt(pmgr.getValue("crawler.core.properties", "mail.smtp.port").trim()));
         }
         if (pmgr.getValue("crawler.core.properties", "mail.smtp.sslport") != null && !pmgr.getValue("crawler.core.properties", "mail.smtp.sslport").trim().isEmpty()) {
-            email.setSSL(true);
+            Logger.putLog("Configurando el sslport " + pmgr.getValue("crawler.core.properties", "mail.smtp.sslport"), MailUtils.class, Logger.LOG_LEVEL_INFO);
+            email.setSSLOnConnect(true);
             email.setSslSmtpPort(pmgr.getValue("crawler.core.properties", "mail.smtp.sslport").trim());
         }
+        Logger.putLog("Configurando TLS a " + pmgr.getValue("crawler.core.properties", "mail.smtp.tls"), MailUtils.class, Logger.LOG_LEVEL_INFO);
+        email.setStartTLSEnabled(Boolean.parseBoolean(pmgr.getValue("crawler.core.properties", "mail.smtp.tls")));
+        email.setStartTLSRequired(Boolean.parseBoolean(pmgr.getValue("crawler.core.properties", "mail.smtp.tls")));
+        email.setSSLCheckServerIdentity(false);
 
         return email;
     }
