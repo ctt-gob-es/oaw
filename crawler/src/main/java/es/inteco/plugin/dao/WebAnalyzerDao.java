8ee4b190c5b26e3483ec9b69a551ce38b9007e81
mikunis <miguel.garcia@fundacionctic.org>
2016-09-02 09:38:27 +0200

    Removes unused Termino class

--- crawler/src/main/java/es/inteco/plugin/dao/WebAnalyzerDao.java
+++ crawler/src/main/java/es/inteco/plugin/dao/WebAnalyzerDao.java
@@ -2,7 +2,6 @@ package es.inteco.plugin.dao;
 
 import es.inteco.common.logging.Logger;
 import es.inteco.plugin.BrokenLinks;
-import es.inteco.plugin.Termino;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,51 +12,6 @@ import java.util.List;
 public final class WebAnalyzerDao {
 
     private WebAnalyzerDao() {
-    }
-
-    public static List<Termino> getTerms(Connection conn) {
-        List<Termino> terms = new ArrayList<>();
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            pstmt = conn.prepareStatement("SELECT t.id_termino, t.termino, ct.porcentaje_normalizado, " +
-                    "c.id_categoria, c.categoria, c.umbral FROM termino t " +
-                    "JOIN categoria_termino ct ON (t.id_termino = ct.id_termino) " +
-                    "JOIN categoria c ON (c.id_categoria = ct.id_categoria);");
-            rs = pstmt.executeQuery();
-
-            while (rs.next()) {
-                Termino termino = new Termino();
-                termino.setId(rs.getLong("id_termino"));
-                termino.setName(rs.getString("termino"));
-                termino.setNormalized_percentage(rs.getFloat("porcentaje_normalizado"));
-                termino.setId_category(rs.getLong("id_categoria"));
-                termino.setName_category(rs.getString("categoria"));
-                termino.setThreshold(rs.getFloat("umbral"));
-                terms.add(termino);
-            }
-        } catch (Exception e) {
-            Logger.putLog("Error al recuperar los terminos", WebAnalyzerDao.class, Logger.LOG_LEVEL_ERROR, e);
-            return null;
-        } finally {
-            if (rs != null) {
-                try {
-                    rs.close();
-                } catch (Exception e) {
-                    Logger.putLog("Error al cerrar el resultset", WebAnalyzerDao.class, Logger.LOG_LEVEL_ERROR, e);
-                }
-            }
-
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (Exception e) {
-                    Logger.putLog("Error al cerrar el preparedStament", WebAnalyzerDao.class, Logger.LOG_LEVEL_ERROR, e);
-                }
-            }
-        }
-
-        return terms;
     }
 
     public static List<String> getCartridgeNames(Connection conn, Long idTracking) {
