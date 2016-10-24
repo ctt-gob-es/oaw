e14efdebd78a87bb874f8e5d274de73aa52e6bc4
mikunis <miguel.garcia@fundacionctic.org>
2016-10-24 15:38:05 +0200

    Updates check data markup on layout table and removes tbody condition as new parser adds this element on all tables

--- intavcore/src/main/java/es/inteco/cyberneko/html/HTMLConfiguration.java
+++ intavcore/src/main/java/es/inteco/cyberneko/html/HTMLConfiguration.java
@@ -11,6 +11,9 @@ public class HTMLConfiguration extends org.cyberneko.html.HTMLConfiguration {
         // No queremos incluir el balanceado de etiquetas
         setFeature(BALANCE_TAGS, balanceTags);
 
+        setFeature("http://apache.org/xml/features/dom/defer-node-expansion", false);
+        setFeature("http://cyberneko.org/html/features/scanner/allow-selfclosing-tags", true);
+
         // No se alteran las mayúsculas ni minúsculas de los nombres de elementos y atributos
         setProperty(NAMES_ELEMS, "match");
         setProperty(NAMES_ATTRS, "no-change");
