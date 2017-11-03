03bc86f53f37a6df55ab5ec5373bc552f76ed657
Alvaro Pelaez <alvaro.pelaez@fundacionctic.org>
2017-11-03 08:35:14 +0100

    Cambio a la hora de detectar enlaces a la página de accesibilidad para excluir aquellos enlaces que apunten a un pdf o doc

---
+++ oaw_2017_2018/intavcore/src/test/java/es/gob/oaw/css/utils/AccesibilityDeclarationCheckUtilsTest.java
@@ -0,0 +1,13 @@
+package es.gob.oaw.css.utils;
+
+import es.gob.oaw.utils.AccesibilityDeclarationCheckUtils;
+
+public class AccesibilityDeclarationCheckUtilsTest {
+	
+	private AccesibilityDeclarationCheckUtils adcUtils;
+	
+	public void testGetDocumentText() {
+		
+	}
+
+}
