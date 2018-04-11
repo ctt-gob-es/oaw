84dbc5d7df14f32c3559157daf6843a092e329f3
alvaropelaez <alvaro.pelaez@fundacionctic.org>
2018-04-11 14:11:51 +0200

    Modificaco check de separación de contenido y presentación para excluir del error los iconos incluídos en after o beferore como unicode

--- oaw_2017_2018/intavcore/src/main/java/es/gob/oaw/css/checks/CSSGeneratedContentDocumentHandler.java
+++ oaw_2017_2018/intavcore/src/main/java/es/gob/oaw/css/checks/CSSGeneratedContentDocumentHandler.java
@@ -31,72 +31,73 @@ import javax.annotation.Nonnull;
 import java.util.List;
 
 /**
- * Clase que comprueba si se genera contenido de cierta longitud mediante las pseudo clases :before o :after
+ * Clase que comprueba si se genera contenido de cierta longitud mediante las
+ * pseudo clases :before o :after
  */
 public class CSSGeneratedContentDocumentHandler extends OAWCSSVisitor {
 
-    private static final String CONTENT_PROPERTY = "content";
+	private static final String CONTENT_PROPERTY = "content";
 
-    private final int allowedChars;
+	private final int allowedChars;
 
-    public CSSGeneratedContentDocumentHandler(final int allowedChars) {
-        this.allowedChars = allowedChars;
-    }
+	public CSSGeneratedContentDocumentHandler(final int allowedChars) {
+		this.allowedChars = allowedChars;
+	}
 
-    @Override
-    public List<CSSProblem> evaluate(final Document document, final CSSResource cssResource) {
-        if (!cssResource.getContent().isEmpty() && !cssResource.isInline()) {
-            try {
-                resource = cssResource;
-                final CascadingStyleSheet aCSS = CSSReader.readFromString(cssResource.getContent(), ECSSVersion.CSS30, new CollectingCSSParseErrorHandler());
-                if (aCSS != null) {
-                    CSSVisitor.visitCSS(aCSS, this);
-                }
-            } catch (Exception e) {
-                Logger.putLog("Error al intentar parsear el CSS", OAWCSSVisitor.class, Logger.LOG_LEVEL_INFO);
-            }
-        }
-        return problems;
-    }
+	@Override
+	public List<CSSProblem> evaluate(final Document document, final CSSResource cssResource) {
+		if (!cssResource.getContent().isEmpty() && !cssResource.isInline()) {
+			try {
+				resource = cssResource;
+				final CascadingStyleSheet aCSS = CSSReader.readFromString(cssResource.getContent(), ECSSVersion.CSS30,
+						new CollectingCSSParseErrorHandler());
+				if (aCSS != null) {
+					CSSVisitor.visitCSS(aCSS, this);
+				}
+			} catch (Exception e) {
+				Logger.putLog("Error al intentar parsear el CSS", OAWCSSVisitor.class, Logger.LOG_LEVEL_INFO);
+			}
+		}
+		return problems;
+	}
 
-    @Override
-    public void onDeclaration(@Nonnull final CSSDeclaration cssDeclaration) {
-        if (isValidMedia() && isPseudoClass() && CONTENT_PROPERTY.equals(cssDeclaration.getProperty())) {
-            final String value = CSSSACUtils.parseLexicalValue(getValue(cssDeclaration));
-            if (cssValueGenerateContent(value)) {
-                if (value.length() > allowedChars) {
-                    getProblems().add(createCSSProblem("", cssDeclaration));
-                }
-            }
-        }
-    }
+	@Override
+	public void onDeclaration(@Nonnull final CSSDeclaration cssDeclaration) {
+		if (isValidMedia() && isPseudoClass() && CONTENT_PROPERTY.equals(cssDeclaration.getProperty())) {
+			final String value = CSSSACUtils.parseLexicalValue(getValue(cssDeclaration));
+			if (cssValueGenerateContent(value)) {
+				if (value.length() > allowedChars && value.length() != 5 && !value.startsWith("\\")) {
+					getProblems().add(createCSSProblem("", cssDeclaration));
+				}
+			}
+		}
 
-    private boolean cssValueGenerateContent(final String cssValue) {
-        if (cssValue == null || cssValue.trim().isEmpty()) {
-            return false;
-        } else {
-            final String loweredCaseValue = cssValue.toLowerCase();
-            if ("none".equals(loweredCaseValue)) {
-                return false;
-            } else if ("open-quote".equals(loweredCaseValue)
-                    || "close-quote".equals(loweredCaseValue)
-                    || "no-open-quote".equals(loweredCaseValue)
-                    || "no-close-quote".equals(loweredCaseValue)) {
-                return false;
-            } else if (loweredCaseValue.startsWith("url")) {
-                return false;
-            } else if (loweredCaseValue.startsWith("counter")) {
-                return false;
-            } else if (loweredCaseValue.startsWith("leader")) {
-                return false;
-            }
-            return true;
-        }
-    }
+	}
 
-    private boolean isPseudoClass() {
-        final String selector = currentStyleRule.getSelectorsAsCSSString(new CSSWriterSettings(ECSSVersion.CSS30), 0);
-        return selector.contains(":before") || selector.contains(":after");
-    }
+	private boolean cssValueGenerateContent(final String cssValue) {
+		if (cssValue == null || cssValue.trim().isEmpty()) {
+			return false;
+		} else {
+			final String loweredCaseValue = cssValue.toLowerCase();
+			if ("none".equals(loweredCaseValue)) {
+				return false;
+			} else if ("open-quote".equals(loweredCaseValue) || "close-quote".equals(loweredCaseValue)
+					|| "no-open-quote".equals(loweredCaseValue) || "no-close-quote".equals(loweredCaseValue)) {
+				return false;
+			} else if (loweredCaseValue.startsWith("url")) {
+				return false;
+			} else if (loweredCaseValue.startsWith("counter")) {
+				return false;
+			} else if (loweredCaseValue.startsWith("leader")) {
+				return false;
+			}
+			return true;
+		}
+	}
+
+	private boolean isPseudoClass() {
+		final String selector = currentStyleRule.getSelectorsAsCSSString(new CSSWriterSettings(ECSSVersion.CSS30), 0);
+		return selector.contains(":before") || selector.contains(":after");
+	}
 
 }
