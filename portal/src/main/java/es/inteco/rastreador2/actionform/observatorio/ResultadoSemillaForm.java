31d2afbdd5b0f74cefca72131195648e3544f49d
Isak Styf <isak.styf@masarinconsulting.se>
2020-04-16 11:25:32 +0200

    Whitespace changes.

--- portal/src/main/java/es/inteco/rastreador2/actionform/observatorio/ResultadoSemillaForm.java
+++ portal/src/main/java/es/inteco/rastreador2/actionform/observatorio/ResultadoSemillaForm.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
-* Copyright (C) 2012 INTECO, Instituto Nacional de Tecnologías de la Comunicación, 
+* Copyright (C) 2012 INTECO, Instituto Nacional de Tecnologías de la Comunicación,
 * This program is licensed and may be used, modified and redistributed under the terms
-* of the European Public License (EUPL), either version 1.2 or (at your option) any later 
+* of the European Public License (EUPL), either version 1.2 or (at your option) any later
 * version as soon as they are approved by the European Commission.
-* Unless required by applicable law or agreed to in writing, software distributed under the 
-* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
-* ANY KIND, either express or implied. See the License for the specific language governing 
+* Unless required by applicable law or agreed to in writing, software distributed under the
+* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+* ANY KIND, either express or implied. See the License for the specific language governing
 * permissions and more details.
-* You should have received a copy of the EUPL1.2 license along with this program; if not, 
+* You should have received a copy of the EUPL1.2 license along with this program; if not,
 * you may find it at http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32017D0863
 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-* Modificaciones: MINHAFP (Ministerio de Hacienda y Función Pública) 
+* Modificaciones: MINHAFP (Ministerio de Hacienda y Función Pública)
 * Email: observ.accesibilidad@correo.gob.es
 ******************************************************************************/
 package es.inteco.rastreador2.actionform.observatorio;
@@ -19,37 +19,37 @@ package es.inteco.rastreador2.actionform.observatorio;
  * The Class ResultadoSemillaForm.
  */
 public class ResultadoSemillaForm {
-    
+
     /** The id. */
     private String id;
-    
+
     /** The name. */
     private String name;
-    
+
     /** The id crawling. */
     private String idCrawling;
-    
+
     /** The id fulfilled crawling. */
     private String idFulfilledCrawling;
-    
+
     /** The active. */
     private boolean active;
-    
+
     /** The score. */
     private String score;
-    
+
     /** The nivel. */
     private String nivel;
-    
+
     /** The id category. */
     private long idCategory;
-    
+
     /** The id ambit. */
     private long idAmbit;
-    
+
     /** The id complexity. */
     private long idComplexity;
-    
+
     /** The compliance. */
     private String compliance;
 
@@ -107,7 +107,7 @@ public class ResultadoSemillaForm {
     public void setIdAmbit(long idAmbit) {
         this.idAmbit = idAmbit;
     }
-    
+
     /**
 	 * Gets the id complexity.
 	 *
@@ -125,7 +125,7 @@ public class ResultadoSemillaForm {
     public void setIdComplexity(long idComplexity) {
         this.idComplexity = idComplexity;
     }
-    
+
     /**
 	 * Gets the id.
 	 *
--- portal/src/main/java/es/inteco/rastreador2/actionform/semillas/CategoriaForm.java
+++ portal/src/main/java/es/inteco/rastreador2/actionform/semillas/CategoriaForm.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
-* Copyright (C) 2012 INTECO, Instituto Nacional de Tecnologías de la Comunicación, 
+* Copyright (C) 2012 INTECO, Instituto Nacional de Tecnologías de la Comunicación,
 * This program is licensed and may be used, modified and redistributed under the terms
-* of the European Public License (EUPL), either version 1.2 or (at your option) any later 
+* of the European Public License (EUPL), either version 1.2 or (at your option) any later
 * version as soon as they are approved by the European Commission.
-* Unless required by applicable law or agreed to in writing, software distributed under the 
-* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF 
-* ANY KIND, either express or implied. See the License for the specific language governing 
+* Unless required by applicable law or agreed to in writing, software distributed under the
+* License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+* ANY KIND, either express or implied. See the License for the specific language governing
 * permissions and more details.
-* You should have received a copy of the EUPL1.2 license along with this program; if not, 
+* You should have received a copy of the EUPL1.2 license along with this program; if not,
 * you may find it at http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32017D0863
 * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-* Modificaciones: MINHAFP (Ministerio de Hacienda y Función Pública) 
+* Modificaciones: MINHAFP (Ministerio de Hacienda y Función Pública)
 * Email: observ.accesibilidad@correo.gob.es
 ******************************************************************************/
 package es.inteco.rastreador2.actionform.semillas;
@@ -28,22 +28,22 @@ import java.util.List;
  * The Class CategoriaForm.
  */
 public class CategoriaForm extends ValidatorForm implements Serializable {
-    
+
     /** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -1752910926145527308L;
 
 	/** The id. */
     private String id;
-    
+
     /** The name. */
     private String name;
-    
+
     /** The file seeds. */
     private FormFile fileSeeds;
-    
+
     /** The seeds. */
     private List<SemillaForm> seeds;
-    
+
     /** The orden. */
     private int orden;
 
@@ -187,7 +187,7 @@ public class CategoriaForm extends ValidatorForm implements Serializable {
 			return false;
 		return true;
 	}
-    
-    
-    
+
+
+
 }
