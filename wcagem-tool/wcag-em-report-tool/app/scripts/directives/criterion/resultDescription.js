3b52c293e0256aedb8aba42be33c443a35133c23
alvaropelaez <alvaro.pelaez@fundacionctic.org>
2020-09-29 12:58:30 +0200

    Fix translate ngModel in resultDescription
    Former-commit-id: 29a204e1a0a87d7c26aa46e3132aa966be8aa3ae

--- wcagem-tool/wcag-em-report-tool/app/scripts/directives/criterion/resultDescription.js
+++ wcagem-tool/wcag-em-report-tool/app/scripts/directives/criterion/resultDescription.js
@@ -1,12 +1,15 @@
 'use strict';
 angular.module('wcagReporter')
-  .directive('resultDescription', function (directivePlugin) {
+  .directive('resultDescription', function (directivePlugin,$translate, $filter) {
     return directivePlugin({
       restrict: 'E',
       replace: true,
       scope: {
         value: '=',
         updateMetadata: '='
+      },
+      link:function(scope){
+        scope.value= $filter('translate')(scope.value);
       },
       templateUrl: 'views/directives/criterion/resultDescription.html'
     });
