c5a2b34bc8f4a7cd271486a997b000f11ac5e8bc
Álvaro Peláez <alvaro.pelaez@fundacionctic.org>
2020-12-09 09:39:34 +0100

    Update basic service mail text
    Update basic service report how to first paragraph

--- common/src/main/java/es/gob/oaw/MailService.java
+++ common/src/main/java/es/gob/oaw/MailService.java
@@ -50,6 +50,16 @@ public class MailService {
 		mailProvider.sendMail();
 	}
 
+	public void sendMail(final List<String> mailTo, final String mailSubject, final String mailBody, final String attachUrl, final String attachName, final boolean html) {
+		final MailProvider mailProvider = MailProviderFactory.getMailProvider(pmgr.getValue(MailProvider.MAIL_PROPERTIES, "mail.transport.protocol"));
+		mailProvider.setSubject(mailSubject);
+		mailProvider.setBody(mailBody);
+		mailProvider.setMailTo(mailTo);
+		mailProvider.setAttachment(attachName, attachUrl);
+		mailProvider.setHtml(html);
+		mailProvider.sendMail();
+	}
+
 	/**
 	 * Send mail.
 	 *
