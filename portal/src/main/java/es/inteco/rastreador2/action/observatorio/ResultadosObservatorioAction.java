7178a4b36c4320e41ae6b04521e5390d0c7916d2
SNasser <salvador.nasser@fundacionctic.org>
2021-02-01 10:15:30 +0100

    Rename method to fix incoherence.

--- portal/src/main/java/es/inteco/rastreador2/action/observatorio/ResultadosObservatorioAction.java
+++ portal/src/main/java/es/inteco/rastreador2/action/observatorio/ResultadosObservatorioAction.java
@@ -400,7 +400,7 @@ public class ResultadosObservatorioAction extends Action {
 			}
 			AnnexUtils.createAnnexPaginas(resources, idObsExecution, idOperation, null, null);
 			AnnexUtils.createAnnexPortales(resources, idObsExecution, idOperation, null, null);
-			AnnexUtils.createAnnexXLSX(resources, idObsExecution, idOperation);
+			AnnexUtils.createAnnexXLSX2(resources, idObsExecution, idOperation);
 			final PropertiesManager pmgr = new PropertiesManager();
 			final String exportPath = pmgr.getValue(CRAWLER_PROPERTIES, "export.annex.path");
 			final String zipPath = exportPath + idOperation + File.separator + "anexos.zip";
