d1cf4826bff74083f46430c050c868a848df7120
Álvaro Peláez <alvaro.pelaez@fundacionctic.org>
2021-02-19 08:49:33 +0100

    UX fixes

--- wcagemtool/app/scripts/directives/evaluate/inputPages.js
+++ wcagemtool/app/scripts/directives/evaluate/inputPages.js
@@ -8,7 +8,8 @@ angular.module('wcagReporter')
         pages: '=',
         addPage: '&',
         removePage: '&',
-        updatePage: '&',
+        updatePage: '&', 
+        suffix:'='
       },
       link: function (scope) {
         var addPageFunc = scope.addPage();
