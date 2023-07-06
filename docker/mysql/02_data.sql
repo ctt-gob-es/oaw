f486a90cc3ecfe124bc179afcb6b27f559a7fe18
MartinLB <martin.laviada@fundacionctic.org>
2023-07-06 15:18:50 +0200

    Sql script refactor

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:033ffb51859bcf04d48e28f072a5d750121f1bdfa449e86d037f52b8712517b8
-size 79870697
+oid sha256:f149b217c48150704cdec66c85581c36d18cc680ea43ec86d29f7c4b136aeaba
+size 79869816
