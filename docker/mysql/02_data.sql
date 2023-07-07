8eea167db25eed19d25c801d12ec90c6233c093f
MartinLB <martin.laviada@gmail.com>
2023-07-07 10:45:45 +0200

    script comments removed

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f149b217c48150704cdec66c85581c36d18cc680ea43ec86d29f7c4b136aeaba
-size 79869816
+oid sha256:f6aefd48d78ea54beda4fb9f82c13d729cb7cf60093d536ce00754d5295db039
+size 79858679
