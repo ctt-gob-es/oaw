dd995f0c21e37805a4e1706cc1e35321fcad5edf
MartinLB <martin.laviada@fundacionctic.org>
2023-07-07 15:25:01 +0200

    First cuts

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8b99c61cd1d3f15513f17f6ab0796ee79ac88f11d3f9a33a33e508d54be4468
-size 61814344
+oid sha256:193df843bdaf3f6afad1e48e37790f942fa67031ec96fbfb3481c77125034518
+size 5416592
