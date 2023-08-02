eb4c0f41abfafd2936e64c3635756414730ca800
victor_cima <victor.cima@fundacionctic.org>
2023-08-03 00:23:39 +0200

    Script update: create table apiKey

--- portal/scripts/5.7.0/ALTER_OAW_5.7.0.sql
+++ portal/scripts/5.7.0/ALTER_OAW_5.7.0.sql
@@ -15,3 +15,15 @@ ADD COLUMN mid_previous_score FLOAT(4,2) NULL DEFAULT NULL;
 
 ALTER TABLE observatorio_send_historic_results
 ADD COLUMN mid_previous_score FLOAT(4,2) NULL DEFAULT NULL;
+
+CREATE TABLE apiKey (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  nombre varchar(100) COLLATE utf8_bin NOT NULL,
+  descripcion varchar(200) COLLATE utf8_bin DEFAULT NULL,
+  apiKey varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  activa bit(1) DEFAULT b'0',
+  PRIMARY KEY (id),
+  KEY id (id)
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
