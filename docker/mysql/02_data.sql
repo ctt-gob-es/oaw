ca9dbb0d8c1dd0d0f4010142cef591d9dd2d756e
MartinLB <martingedo@gmail.com>
2023-08-03 10:45:57 +0200

    Demo Fixed
    Report generation still failing

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4152a33644ba723f3060f42d53e8d24aca4053af0d584e581ec2894fe2f4495d
-size 8489
+oid sha256:0e50e83600bfc31b61538f25b99bcac62bc031d65921dc83e4947a33af39c5c2
+size 8671
