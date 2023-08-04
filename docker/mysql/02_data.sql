a6f79c9e515662a599314d9833a3493573b6c230
MartinLB <martingedo@gmail.com>
2023-08-04 09:02:44 +0200

    Personal data removed from data script SQL

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6abe2f4d751c5cd72e1404ac5c301f75b9d34560a6284bbd5e80ec803639f72
-size 21415277
+oid sha256:de332e09617e36aee49feb3633103c96805b47f00559b4b2161e79a7ad8ffbb1
+size 21415209
