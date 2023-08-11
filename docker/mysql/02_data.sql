1e9ecfa9900d52c8138844e40d35c0723ce096b6
MartinLB <martingedo@gmail.com>
2023-08-11 09:07:11 +0200

    Without Templates

--- docker/mysql/02_data.sql
+++ docker/mysql/02_data.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de332e09617e36aee49feb3633103c96805b47f00559b4b2161e79a7ad8ffbb1
-size 21415209
+oid sha256:218fb0fbd4855bc0091954e711321719fb15df0d8d89e28f68e519c58a09b73d
+size 8852
